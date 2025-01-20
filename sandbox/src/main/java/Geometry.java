f0e1bc07344263f54775156a03c5e4d5d1f319ec
Alex Pytyev <sfunx+github@live.ru>
2025-01-20 10:47:09 +0000

    Experimenting with methods
    Added methods for squares and rectangles
    Todo: get acquainted with private and public modifiers

--- sandbox/src/main/java/Geometry.java
+++ sandbox/src/main/java/Geometry.java
@@ -3,20 +3,33 @@ import java.io.OutputStreamWriter;
 import java.nio.charset.Charset;
 
 public class Geometry {
-    private static String getDefaultCharSet() {
-        OutputStreamWriter writer = new OutputStreamWriter(new ByteArrayOutputStream());
-        String enc = writer.getEncoding();
-        return enc;
-    }
+//    private static String getDefaultCharSet() {
+//        OutputStreamWriter writer = new OutputStreamWriter(new ByteArrayOutputStream());
+//        String enc = writer.getEncoding();
+//        return enc;
+//    }
     public static void main(String[] args) {
-        var a = 5;
+        printSquareArea( 7);
+        printSquareArea( 5);
+        printSquareArea( 3);
 
-//        Charset.defaultCharset()
-//        System.out.println("Default Charset=" + Charset.defaultCharset());
-//        System.setProperty("file.encoding", "CP1251");
-//        System.out.println("file.encoding=" + System.getProperty("file.encoding"));
-//        System.out.println("Default Charset=" + Charset.defaultCharset());
-//        System.out.println("Default Charset in Use=" + getDefaultCharSet());
-        System.out.println("Площадь квадрата со стороной " + a + " = " + a * a);
+        printRectangleArea( 4.0, 5.0 );
+        printRectangleArea( 5.0, 7.0 );
+    }
+
+    static void printRectangleArea(double a, double b) {
+        System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + squareRectangle(a,b));
+    }
+
+    private static double squareRectangle(double a, double b) {
+        return a * b;
+    }
+
+    static void printSquareArea(double side) {
+        System.out.println("Площадь квадрата со стороной " + side + " = " + squareArea(side));
+    }
+
+    private static double squareArea(double side) {
+        return side * side;
     }
 }
