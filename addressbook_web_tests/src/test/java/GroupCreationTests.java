8d4ac4d0f4021a1b22589dfab17bea07f1912380
Alex Pytyev <sfunx+github@live.ru>
2025-02-02 20:30:54 +0000

    Added GroupData class
    Added class GroupData, as per lection https://software-testing.ru/lms/mod/page/view.php?id=468140

--- addressbook_web_tests/src/test/java/GroupCreationTests.java
+++ addressbook_web_tests/src/test/java/GroupCreationTests.java
@@ -1,3 +1,4 @@
+import model.GroupData;
 import org.junit.jupiter.api.Test;
 
 public class GroupCreationTests extends TestBase {
@@ -5,12 +6,18 @@ public class GroupCreationTests extends TestBase {
     @Test
     public void canCreateAGroup() {
         openGroupsPage();
-        createGroup("new group1", "group header1", "group footer1");
+        createGroup(new GroupData("new group1", "name header1", "name footer1"));
     }
 
     @Test
     public void canCreateGroupWithEmptyName() {
         openGroupsPage();
-        createGroup("", "", "");
+        createGroup(new GroupData());
+    }
+
+    @Test
+    public void canCreateGroupWithNameOnly() {
+        openGroupsPage();
+        createGroup(new GroupData().withName("some name"));
     }
 }
